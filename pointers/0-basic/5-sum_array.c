9d2d5b4bba34fabfcdc23b2b6f113e8b80f00649
Vojtech Maly <voj.maly@gmail.com>
2025-09-18 05:52:55 +0000

    Initial commit

---
+++ pointers/0-basic/5-sum_array.c
@@ -0,0 +1,31 @@
+/*
+Pass Array to Function Using Pointer
+You pass the array to a function using a pointer.
+
+Inside the function, you perform operations like summing all elements.
+
+You return the result to the main function.
+*/
+
+#include <stdio.h>
+
+int arr[] = {1, 2, 3, 4, 5, 6};
+int* ptr_arr;
+
+
+int sum(int* input) {
+    int ret_val = 0;
+    int arr_lenght = sizeof(input) / input[0];
+    for (int i = 0; i < arr_lenght; i++) {
+     // ret_val = ret_val + input[i] same as under
+        ret_val += input[i];
+    }
+    return ret_val;
+}
+
+
+int main() {
+    ptr_arr = arr;
+    printf("Sum aof elements in array using function is: %d\n", sum(*ptr_arr));
+    return 0;
+}
